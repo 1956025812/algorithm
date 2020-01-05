65b654ebc7d21b1e86a14472b48bc50f43c4c927
1956025812 <1956022812@qq.com>
2020-01-05 20:24:02 +0800

    算法1

--- cxydmmszn/src/main/java/day20200101/MyMinStack.java
+++ cxydmmszn/src/main/java/day20200101/MyMinStack.java
@@ -7,65 +7,72 @@ import java.util.Stack;
  * 要求：
  * 1. pop、push、getMin的时间复杂度都是o(1)
  * 2. 设计的栈类型可以使用现成的栈结构
+ * <p>
+ * 思路：
+ * 构造一个临时的栈，用来存放最小的数据。
  */
 public class MyMinStack {
 
-    private Stack<Integer> stackData;
-    private Stack<Integer> statckMin;
+    private Stack<Integer> dataStack;
+    private Stack<Integer> tempStack;
 
     public MyMinStack() {
-        this.stackData = new Stack<>();
-        this.statckMin = new Stack<>();
+        this.dataStack = new Stack<>();
+        this.tempStack = new Stack<>();
+    }
+
+    /**
+     * push方法
+     *
+     * @param data 新数据
+     */
+    public void push(Integer data) {
+        dataStack.push(data);
+
+        if (tempStack.isEmpty()) {
+            tempStack.push(data);
+        } else {
+            if (data < tempStack.peek()) {
+                tempStack.push(data);
+            }
+        }
     }
 
 
-    public void push(Integer newNum) {
-        this.stackData.push(newNum);
-
-        if(this.statckMin.isEmpty()) {
-            this.statckMin.push(newNum);
-        } else if(newNum <= this.getMin()) {
-            this.statckMin.push(newNum);
+    /**
+     * pop方法
+     *
+     * @return 栈顶元素
+     */
+    public Integer pop() {
+        if(dataStack.isEmpty()) {
+            throw new RuntimeException("当前栈dataStata没有数据");
         }
+        return dataStack.pop();
     }
 
 
-    public int pop() {
-        if(this.stackData.isEmpty()) {
-            throw new RuntimeException("yout statck is empty.");
+    /**
+     * pop方法
+     *
+     * @return 栈的最小元素
+     */
+    public Integer getMin() {
+        if(tempStack.isEmpty()) {
+            throw new RuntimeException("当前栈dataStata没有数据");
         }
-
-        int value = this.stackData.pop();
-        if(value == this.getMin()) {
-            this.statckMin.pop();
-        }
-        return value;
-    }
-
-
-    public int getMin() {
-        if(this.statckMin.isEmpty()) {
-            throw new RuntimeException("yout stack is empty.");
-        }
-        return this.statckMin.peek();
+        return tempStack.pop();
     }
 
 
     public static void main(String[] args) {
-
         MyMinStack myMinStack = new MyMinStack();
-        myMinStack.push(4);
         myMinStack.push(3);
         myMinStack.push(2);
+        myMinStack.push(3);
         myMinStack.push(6);
-        System.out.println(myMinStack);
-
-        int minValue = myMinStack.getMin();
-        System.out.println(minValue);
-
+        System.out.println(myMinStack.getMin());
     }
-
-
 }
 
 
