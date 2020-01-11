3870aa009d5eeebb5ceded25dbda4dd04d15ecda
1956025812 <1956022812@qq.com>
2020-01-11 20:58:12 +0800

    算法1

--- cxydmmszn/src/main/java/day20200101/MyMinStack.java
+++ cxydmmszn/src/main/java/day20200101/MyMinStack.java
@@ -10,7 +10,7 @@ import java.util.Stack;
  * <p>
  * 思路：
  * 构造一个临时的栈，用来存放最小的数据。
- * push数据的时候与临时栈的栈顶元素做比较，如果小，就放到临时栈
+ * push数据的时候与临时栈的栈顶元素做比较，如果小于或等于，就放到临时栈。注意： 小于和等于都要放。
  * pop数据的时候与临时栈的栈顶元素做比较，如果一样，则同步pop
  */
 public class MyMinStack {
@@ -34,7 +34,7 @@ public class MyMinStack {
         if (tempStack.isEmpty()) {
             tempStack.push(data);
         } else {
-            if (data < tempStack.peek()) {
+            if (data <= tempStack.peek()) {
                 tempStack.push(data);
             }
         }
@@ -47,13 +47,13 @@ public class MyMinStack {
      * @return 栈顶元素
      */
     public Integer pop() {
-        if(dataStack.isEmpty()) {
+        if (dataStack.isEmpty()) {
             throw new RuntimeException("当前栈dataStata没有数据");
         }
 
         // 如果要pop的元素正好是最小元素，则要同时一起将临时栈的元素pop
         Integer popData = dataStack.peek();
-        if(popData.equals(tempStack.peek())) {
+        if (popData.equals(tempStack.peek())) {
             tempStack.pop();
         }
 
@@ -67,7 +67,7 @@ public class MyMinStack {
      * @return 栈的最小元素
      */
     public Integer getMin() {
-        if(tempStack.isEmpty()) {
+        if (tempStack.isEmpty()) {
             throw new RuntimeException("当前栈dataStata没有数据");
         }
         return tempStack.pop();
